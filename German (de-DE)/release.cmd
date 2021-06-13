dfeec88d1371abb273d9575a3ab6b56a8adc060d
Shaun Walker <shaun.walker@siliqon.com>
2021-06-13 13:38:29 -0400

    fix paths

--- German (de-DE)/release.cmd
+++ German (de-DE)/release.cmd
@@ -1,6 +1,6 @@
-dotnet clean -c Release ..\..\..\Oqtane.sln 
 dotnet build -c Release ..\..\..\Oqtane.sln
-copy ..\..\bin\Release\net5.0\de-DE\Oqtane.Client.resources.dll ..\..\..\Oqtane.Server\net5.0\de-DE\Oqtane.Client.resources.dll
+mkdir ..\..\..\Oqtane.Server\bin\Debug\net5.0\de-DE
+copy ..\..\..\Oqtane.Server\bin\Release\net5.0\de-DE\Oqtane.Client.resources.dll ..\..\..\Oqtane.Server\bin\Debug\net5.0\de-DE\Oqtane.Client.resources.dll
 ..\..\..\Oqtane.Package\nuget.exe pack Oqtane.Client.de-DE.nuspec
 pause 
 
