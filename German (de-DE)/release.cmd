101e25e24ca0695e566ff21e0c06639b746a1417
Shaun Walker <shaun.walker@siliqon.com>
2021-06-14 16:52:00 -0400

    update paths

--- German (de-DE)/release.cmd
+++
@@ -1,6 +0,0 @@
-dotnet build -c Release ..\..\..\Oqtane.sln
-mkdir ..\..\..\Oqtane.Server\bin\Debug\net5.0\de-DE
-copy ..\..\..\Oqtane.Server\bin\Release\net5.0\de-DE\Oqtane.Client.resources.dll ..\..\..\Oqtane.Server\bin\Debug\net5.0\de-DE\Oqtane.Client.resources.dll
-..\..\..\Oqtane.Package\nuget.exe pack Oqtane.Client.de-DE.nuspec
-pause 
-
